0bb79c2bb595334e7ff9dffdd0d200574e313897
Jean-Luc Martin <jean-luc.martin@kandy.io>
2019-03-30 13:23:22 -0400

    remove semi colons

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const tmi = require('tmi.js');
+const tmi = require('tmi.js')
 
 // Define configuration options
 const opts = {
@@ -9,42 +9,42 @@ const opts = {
   channels: [
     'brmariomarathon'
   ]
-};
+}
 
 // Create a client with our options
-const client = new tmi.client(opts);
+const client = new tmi.client(opts)
 
 // Register our event handlers (defined below)
-client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
+client.on('message', onMessageHandler)
+client.on('connected', onConnectedHandler)
 
 // Connect to Twitch:
-client.connect();
+client.connect()
 
 // Called every time a message comes in
 function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
+  if (self) { return } // Ignore messages from the bot
 
   // Remove whitespace from chat message
-  const commandName = msg.trim();
+  const commandName = msg.trim()
 
   // If the command is known, let's execute it
   if (commandName === '!dice') {
-    const num = rollDice();
-    client.say(target, `You rolled a ${num}`);
-    console.log(`* Executed ${commandName} command`);
+    const num = rollDice()
+    client.say(target, `You rolled a ${num}`)
+    console.log(`* Executed ${commandName} command`)
   } else {
-    console.log(`* Unknown command ${commandName}`);
+    console.log(`* Unknown command ${commandName}`)
   }
 }
 
 // Function called when the "dice" command is issued
 function rollDice () {
-  const sides = 6;
-  return Math.floor(Math.random() * sides) + 1;
+  const sides = 6
+  return Math.floor(Math.random() * sides) + 1
 }
 
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler (addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
+  console.log(`* Connected to ${addr}:${port}`)
 }
